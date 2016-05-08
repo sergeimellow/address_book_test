e0ad324db07b90eace764e5a135760ecfa519226
Sergei Mellow <sergei.mellow@gmail.com>
2016-05-08 13:41:41 -0400

    finished up with functionality, time for styling

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,15 @@
 class ContactsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:new, :create, :show, :edit, :update, :destroy]
 
 
   def index
-    book = Book.find(params[:book_id])
-    redirect_to book
+    redirect_to @book
   end
 
   def new
-    #1st you retrieve the post thanks to params[:post_id]
-    book = Book.find(params[:book_id])
-    #2nd you build a new one
-    @contact = book.contacts.build
-
+    @contact = @book.contacts.build
   end
 
   def create
@@ -21,8 +17,9 @@ class ContactsController < ApplicationController
     updated_params['phone_numbers']=updated_params['phone_numbers'].split(',')
     updated_params['email_addresses']=updated_params['email_addresses'].split(',')
     @contact = Contact.new(updated_params)
+    @contact.book = @book
     if @contact.save
-      redirect_to @contact
+      redirect_to @book
     else
       redirect_to root_path
     end
@@ -32,21 +29,32 @@ class ContactsController < ApplicationController
     
   end
 
+  def edit
+  end
+
   def update
     updated_params = contact_params
     updated_params['phone_numbers']=updated_params['phone_numbers'].split(',')
     updated_params['email_addresses']=updated_params['email_addresses'].split(',')
+
     if @contact.update(updated_params)
       redirect_to @contact.book, notice: 'Updated contact'
     else
       render action: 'edit'
     end
   end
-
+  def destroy
+    @contact.destroy
+    redirect_to @book
+  end
 
   private
     def set_contact
       @contact = Contact.find(params[:id])
+    end
+
+    def set_book
+      @book = Book.find(params[:book_id])
     end
   
     def contact_params
